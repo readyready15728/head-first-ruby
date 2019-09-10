1a0489404877c2fb8ba71642b3b55b9790eebb70
Lynn Bradshaw <readyready15728@gmail.com>
2019-09-09 22:50:30 -0400

    Adding car-and-engine.rb

---
+++ ch04/car-and-engine.rb
@@ -0,0 +1,23 @@
+class Engine
+  def initialize(sound='Vroom!!!')
+    @sound = sound
+  end
+
+  def make_sound
+    puts @sound
+  end
+end
+
+class Car
+  def initialize(engine)
+    @engine = engine
+  end
+
+  def rev_engine
+    @engine.make_sound
+  end
+end
+
+engine = Engine.new
+car = Car.new(engine)
+car.rev_engine
