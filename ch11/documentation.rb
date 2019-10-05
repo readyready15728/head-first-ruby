3aaf3b06bebc2adc91d42f79c6e328d16d751cfd
Lynn Bradshaw <readyready15728@gmail.com>
2019-10-05 00:21:01 -0400

    Adding documentation.rb

---
+++ ch11/documentation.rb
@@ -0,0 +1,10 @@
+array = [10, 5, 7, 3, 9]
+
+first = array.first
+puts "We pulled #{first} off the start of the array."
+
+last = array.last
+puts "We pulled #{last} off the end of the array."
+
+largest = array.max
+puts "The largest remaining number is #{largest}."
