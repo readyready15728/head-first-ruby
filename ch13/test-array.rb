1a5b923edee396d9badfaa1ac46293912744c141
Lynn Bradshaw <readyready15728@gmail.com>
2019-10-06 02:44:46 -0400

    Adding test-array.rb

---
+++ ch13/test-array.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+
+class TestArray < MiniTest::Test
+  def setup
+    @array = ['a', 'b', 'c']
+  end
+
+  def test_length
+    assert_equal(3, @array.length)
+  end
+  
+  def test_last
+    assert_equal('c', @array.last)
+  end
+
+  def test_join
+    assert_equal('a-b-c', @array.join('-'))
+  end
+end
