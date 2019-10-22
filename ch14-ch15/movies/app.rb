f2426fd0f973e95a3891d6da607b22a7a0ffbb90
Lynn Bradshaw <readyready15728@gmail.com>
2019-10-22 06:41:26 -0400

    Adding temperature conversion exercise

--- ch14-ch15/movies/app.rb
+++ ch14-ch15/movies/app.rb
@@ -41,3 +41,13 @@ get('/multiplication') do
 
   erb :multiplication
 end
+
+get('/form') do
+  erb :form
+end
+
+post('/convert') do
+  fahrenheit = params['temperature'].to_f
+  celsius = (fahrenheit - 32) / 1.8
+  '%0.1f degrees Fahrenheit is %0.1f degrees Celsius.' % [fahrenheit, celsius]
+end
